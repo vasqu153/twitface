34f588d44fbdc65ddee432f69afa6bd28cfb9121
Gaby Ghermezi <gherm101@mail.chapman.edu>
2018-11-13 22:18:56 -0800

    Update twitface.py
    Pre-generated posts were created for each category as well as verifying that the user wants to post the status

--- twitface.py
+++ twitface.py
@@ -5,7 +5,7 @@
 # Co-Author: Gabriela Ghermezi
 # Email: gherm101@mail.chapman.edu
 # Course: CPSC 353
-# Version 1.2
+# Version 1.3
 # Date: November 11, 2018
 #
 # This program should prompt user to enter their twitter and facebook login
@@ -19,26 +19,26 @@
 
 # import twitter #hidden until needed for code
 # import json #hidden until needed
-
 print "---------------------------------------------------------------------"
 print ''
 print "Welcome to TwitFace!"
-print ''
-#print"Please enter your login credentials for Twitter: "
-#print
-#print "Username: "
-#print "Password: "
-#print
-#print "Login successful"
-
+print ''  
+    #print"Please enter your login credentials for Twitter: "
+    #print
+    #print "Username: "
+    #print "Password: "
+    #print
+    #print "Login successful"
 print "---------------------------------------------------------------------"
 print ''
 print "Here you can either post to twitter with a status of your own, or you can choose from a collection we have provided."
 print ''
 choice = 0
 status = ""
+
+print ""
 print "Enter 1 to create your own post, or 2 to select from a list"
-print ''
+print ""
 
 choice = int(input("Choice: "))
 print ""
@@ -55,24 +55,37 @@ elif choice == 2:
         print "4: Good Luck Post"
         choice_2 = int(input())
         if choice_2 == 1:
-            status = ""
+            status = "Having a great day so far! #happy"
             break
         elif choice_2 == 2:
-            status = ""
+            status = "Wish I had a better day today #sad"
             break
         elif choice_2 == 3:
-            status = ""
+            status = "Studied all night and my test was postponed! #angry"
             break
         elif choice_2 == 4:
-            status = ""
+            status = "#GoodLuck on midterms everyone!"
             break
         else:
             print "Invalid Entry! Try Again!"
 
 print ""
-print "Here's your status update"
+print "Are you sure you want to post this status?"
 print ""
-print(status)
+
+while True:
+        # main program
+        while True:
+            answer = raw_input('(y/n): ')
+            if answer in ('y'):
+                print ""
+                print 'Your status has successfully been posted'
+                print ""
+            if answer == 'y':
+                exit()
+# loops back to beginning to reprompt user input
+            if answer in ('n'):
+                exit()
 
 print "---------------------------------------------------------------------"
 
